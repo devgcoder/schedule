bce4ba0af6eb37a069c0f4d905f1466be4cd6c47
devgcoder <29835470+devgcoder@users.noreply.github.com>
2021-04-24 02:35:29 +0800

    Add files via upload
    init project

---
+++ src/main/java/com/github/devgcode/schedule/DevgScheduleAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.github.devgcode.schedule;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication
+public class DevgScheduleAutoConfiguration {
+
+
+}
